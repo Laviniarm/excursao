3fdabacbd683876729ab8affe76aa8cd36ebb256
Lavínia Rodrigues <lavinia.rodriguesr@gmail.com>
2023-09-28 09:26:09 -0300

    Update Teste.java
    Teste

--- APP/src/Teste.java
+++ APP/src/Teste.java
@@ -3,14 +3,14 @@ public class Teste {
 		try {
 			
 			Excursao e1 = new Excursao(1, 15, 5);
-			e1.reservar("123", "raiza");
-			e1.reservar("245", "lucas");
-			e1.reservar("245", "maria");
-			e1.reservar("123", "godofredo");
+			e1.criarReserva("123", "raiza");
+			e1.criarReserva("245", "lucas");
+			e1.criarReserva("245", "maria");
+			e1.criarReserva("123", "godofredo");
 			//e1.reservar("123", "godofredo");
 			
 			Excursao e2 = new Excursao(2, 25, 3);
-			e2.reservar("456", "raizinha");
+			e2.criarReserva("456", "raizinha");
 			
 			e1.cancelarIndividualmente("123", "raiza");
 			e1.cancelarCPF("245");
@@ -18,13 +18,8 @@ public class Teste {
 			System.out.println(e1.listarReservasPorCpf("123"));
 			System.out.println(e2.listarReservasPorNome("raizinha"));
 
-			
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 
-			excursao.criarReserva("111", "joao");
+			e1.criarReserva("111", "joao");
 			excursao.criarReserva("222", "maria");
 			excursao.criarReserva("333", "jose");
 			excursao.criarReserva("333", "paulo");
@@ -46,7 +41,7 @@ public class Teste {
 			System.out.println(excursao.listarReservasPorCpf(""));
 			System.out.println("\nexcursao:" + excursao);
 			System.out.println("\ntotal=" + excursao.calcularValorTotal());
-		} 
+		}
 		catch (Exception erro) {
 			System.out.println("-->" + erro.getMessage());
 		}
