fa2d3726a4635772e47217e213b8658f44d7f1d3
Raiza <ftraiza@gmail.com>
2023-09-27 21:27:36 -0300

    início da interface gráfica

--- APP/src/Teste.java
+++ APP/src/Teste.java
@@ -1,55 +1,13 @@
 public class Teste {
 	public static void main(String[] args) {
 		try {
-			
-			Excursao e1 = new Excursao(1, 15, 5);
-			e1.reservar("123", "raiza");
-			e1.reservar("245", "lucas");
-			e1.reservar("245", "maria");
-			e1.reservar("123", "godofredo");
-			//e1.reservar("123", "godofredo");
-			
-			Excursao e2 = new Excursao(2, 25, 3);
-			e2.reservar("456", "raizinha");
-			
-			e1.cancelarIndividualmente("123", "raiza");
-			e1.cancelarCPF("245");
-			
-			System.out.println(e1.listarReservasPorCpf("123"));
-			System.out.println(e2.listarReservasPorNome("raizinha"));
-
-			
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-			excursao.criarReserva("111", "joao");
-			excursao.criarReserva("222", "maria");
-			excursao.criarReserva("333", "jose");
-			excursao.criarReserva("333", "paulo");
-			excursao.criarReserva("333", "ana");
-			excursao.criarReserva("555", "antonio");
-			excursao.criarReserva("555", "joana");
-
-			System.out.println("\nlistar todas as reservas");
+			Excursao excursao = new Excursao(1234);			
+			excursao.carregar();
 			System.out.println(excursao.listarReservasPorCpf(""));
-			System.out.println("\nlistar as reservas por cpf");
-			System.out.println(excursao.listarReservasPorCpf("3"));
-			System.out.println("\nlistar as reservas por nome");
-			System.out.println(excursao.listarReservasPorNome("jo"));
-
-			excursao.cancelarReserva("555", "claudia");
-			excursao.cancelarReserva("333");
-
-			System.out.println("\nlistar todas as reservas");
-			System.out.println(excursao.listarReservasPorCpf(""));
-			System.out.println("\nexcursao:" + excursao);
 			System.out.println("\ntotal=" + excursao.calcularValorTotal());
 		} 
 		catch (Exception erro) {
 			System.out.println("-->" + erro.getMessage());
 		}
-
 	}
 }
